b0bfca403857c03efffa5b1f7cd568695bbe181e
Mario <tmario291@gmail.com>
2026-06-10 13:42:10 -0600

    Primer commit

---
+++ app/Models/Organization.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Organization extends Model
+{
+    use HasUlids;
+
+    /**
+     * @var list<string>
+     */
+    protected $fillable = [
+        'owner_id',
+        'name',
+        'slug',
+        'description',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'state',
+        'logo',
+        'status',
+        'settings',
+    ];
+
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'settings' => 'array',
+        ];
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
